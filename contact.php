8aae9d6e4512d1e983d9d13e82f70fcd1c019536
CodeServHost <114128465+CodeServHost@users.noreply.github.com>
2023-05-27 10:02:45 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -64,7 +64,87 @@
 <body class="container-1340px">
 	<div id="wrapper" class="clearfix">
 
-		<?php include "includes/header.php" ?>
+		<!-- Header -->
+	<header id="header" class="header header-layout-type-header-2rows">
+		<div class="header-top ">
+			<div class="container">
+				<div class="row">
+					<div class="col-xl-auto header-top-left align-self-center text-center text-xl-left">
+						<ul class="element contact-info">
+							<li class="contact-phone"><i class="fa fa-phone font-icon sm-display-block"></i> Tel: +44 01604600668 </li>
+							<li class="contact-email"><i class="fa fa-envelope-o font-icon sm-display-block"></i> kasturitraders65@gmail.com</li>
+							<li class="contact-address"><i class="fa fa-map-o font-icon sm-display-block"></i>Billing Brook Rd, Northampton, NN3 8AA </li>
+						</ul>
+					</div>
+					<div class="col-xl-auto ml-xl-auto header-top-right align-self-center text-center text-xl-right">
+						<div class="element">
+							<ul class="header-top-nav list-inline">
+								<li class="menu-item"><a title="Home" class="menu-item-link" href="index.php"><strong>Home</strong></a></li>
+								<li class="menu-item"><a title="Home" class="menu-item-link" href="contact.php"><strong>Contact Us</strong></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="header-nav">
+			<div class="header-nav-wrapper navbar-scrolltofixed green">
+				<div class="menuzord-container header-nav-container green ">
+					<div class="container position-relative">
+						<div class="row">
+							<div class="col">
+								<div class="row header-nav-col-row">
+									<div class="col-sm-auto align-self-center">
+										<a class="menuzord-brand site-brand" href="index.php">
+											<span>C</span>-Mobile<!-- 
+											<img class="logo-default logo-1x" src="images/logo-wide.png" alt="Logo">
+											<img class="logo-default logo-2x retina" src="images/logo-wide%402x.png" alt="Logo"> -->
+										</a>
+									</div>
+									<div class="col-sm-auto ml-auto pr-0 align-self-center">
+										<nav id="top-primary-nav" class="menuzord green" data-effect="fade" data-animation="none" data-align="right">
+											<ul id="main-nav" class="menuzord-menu">
+												<li class="active"><a href="index.php">Home</a></li>
+												<li><a href="about.php">About</a></li>
+												<!-- <li><a href="#">Pages</a>
+													<ul class="dropdown">
+														<li><a href="page-gallery.html">Gallery</a></li>
+														<li><a href="page-team-grid.html">Team Grid</a></li>
+														<li><a href="page-team-details.html">Team Details</a></li>
+													</ul>
+												</li>
+												<li><a href="#home">Service</a>
+													<ul class="dropdown">
+														<li><a href="page-services.html">Service Grid</a></li>
+														<li><a href="page-service-details.html">Service Deatils</a></li>
+													</ul>
+												</li>
+												<li><a href="#">Blog</a>
+													<ul class="dropdown">
+														<li><a href="news-grid.html">News Grid</a></li>
+														<li><a href="news-details.html">News Details</a></li>
+													</ul>
+												</li> -->
+												<li><a href="contact.php">Contact</a></li>
+											</ul>
+										</nav>
+									</div>
+								</div>
+								<div class="row d-block d-xl-none">
+									<div class="col-12">
+										<nav id="top-primary-nav-clone" class="menuzord d-block d-xl-none default menuzord-color-default menuzord-border-boxed menuzord-responsive" data-effect="slide" data-animation="none" data-align="right">
+											<ul id="main-nav-clone" class="menuzord-menu menuzord-right menuzord-indented scrollable">
+											</ul>
+										</nav>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
 		<!-- Start main-content -->
 		<div class="main-content">
 			<!-- Section: home -->
